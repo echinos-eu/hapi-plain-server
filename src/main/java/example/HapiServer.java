753b2c88858a5843ad7f473a92f38e725ed75441
Patrick Werner <pa.f.werner@gmail.com>
2024-10-11 15:59:15 +0200

    simple implementierung

--- src/main/java/example/HapiServer.java
+++ src/main/java/example/HapiServer.java
@@ -4,6 +4,7 @@ import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.rest.server.RestfulServer;
 import ca.uhn.fhir.rest.server.interceptor.ResponseHighlighterInterceptor;
 
+import example.provider.PatientProvider;
 import jakarta.servlet.annotation.WebServlet;
 
 @WebServlet("/*")
@@ -12,10 +13,11 @@ public class HapiServer extends RestfulServer {
   public HapiServer() {
     super(FhirContext.forR4());
   }
+
   @Override
   public void initialize() {
     // add provider
-
+    registerProvider(new PatientProvider());
     //Interceptoren
     registerInterceptor(new ResponseHighlighterInterceptor());
   }
