27b38c437edce183d295912a606d27e4c2e2f613
Patrick Werner <pa.f.werner@gmail.com>
2025-03-27 14:47:09 +0100

    search ready

--- src/main/java/example/provider/PatientProvider.java
+++ src/main/java/example/provider/PatientProvider.java
@@ -1,10 +1,18 @@
 package example.provider;
 
 import ca.uhn.fhir.rest.annotation.Create;
+import ca.uhn.fhir.rest.annotation.IdParam;
+import ca.uhn.fhir.rest.annotation.OptionalParam;
+import ca.uhn.fhir.rest.annotation.Read;
 import ca.uhn.fhir.rest.annotation.ResourceParam;
+import ca.uhn.fhir.rest.annotation.Search;
 import ca.uhn.fhir.rest.api.MethodOutcome;
+import ca.uhn.fhir.rest.param.StringParam;
 import ca.uhn.fhir.rest.server.IResourceProvider;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.r4.model.IdType;
@@ -29,5 +37,28 @@ public class PatientProvider implements IResourceProvider {
     MethodOutcome methodOutcome = new MethodOutcome();
     methodOutcome.setId(new IdType("Patient", id));
     return methodOutcome;
+  }
+
+  @Read
+  public Patient read(@IdParam IdType id) {
+    Patient patient = null;
+    patient = patients.get(id.getIdPart());
+    return patient;
+  }
+
+  @Search
+  public List<Patient> searh(
+      @OptionalParam(name = Patient.SP_FAMILY) StringParam familyName) {
+    List<Patient> returnPats = null;
+    if (Objects.isNull(familyName)) {
+      returnPats = new ArrayList<>(patients.values());
+    } else {
+      returnPats = patients.values().stream()
+          .filter(p -> p.getName().stream()
+              .anyMatch(n -> n.getFamily() != null && n.getFamily().startsWith(familyName.getValue())))
+          .toList();
+
+    }
+    return returnPats;
   }
 }
